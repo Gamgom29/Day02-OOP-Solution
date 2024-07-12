31c5255a012dc7b8426cd4bbd91a22e1120eef04
Gamgom29 <abdogamgom2015@gmail.com>
2024-07-12 18:53:37 +0300

    Q03 Part02

--- Assignment/Employee.cs
+++ Assignment/Employee.cs
@@ -7,11 +7,11 @@ using System.Threading.Tasks;
 namespace Assignment
 {
     [Flags]
-    enum SecurityLevels
+    enum SecurityLevels:byte
     {
         Guest = 1 , Developer = 2 , Secertary = 4 , DBA = 8
     }
-    enum Gender
+    enum Gender:byte
     {
         M = 1 ,Male = 1, F = 2 , Female = 2 
     }
@@ -24,14 +24,21 @@ namespace Assignment
         public HiringDate HiringDate { get; set; }
         public Gender Gender { get; set; }
 
-        public Employee(int Id , string Name , decimal Salary , Gender Gender)
+        // Full Privileges Ctor
+        public Employee(int Id, string Name, decimal Salary, Gender Gender):this(Id, Name, Salary, Gender,(SecurityLevels) 15)
+        {
+            
+        }
+
+        // Full Constructor
+        public Employee(int Id , string Name , decimal Salary , Gender Gender , SecurityLevels Privileges)
         {
             this.Id = Id;
             this.Name = Name;
             this.Salary = Salary;
             this.Gender = Gender;
             this.HiringDate = new HiringDate(DateTime.Now.Day , DateTime.Now.Month , DateTime.Now.Year);
-            this.SecurityLevel |= SecurityLevels.Guest | SecurityLevels.DBA | SecurityLevels.Developer | SecurityLevels.Secertary;
+            this.SecurityLevel = Privileges;
         }
 
         public override string ToString()
--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -12,6 +12,46 @@
             #region Q02
 
             #endregion
+
+            #region Q03
+            Employee[] Employees = new Employee [3];
+
+            for (int i = 0; i < Employees.Length; i++) 
+            {
+                Console.WriteLine($"Enter Employee {i+1} Data ");
+                Console.WriteLine();
+                Console.Write($"Enter Employee {i+1} Id ");
+                int id = int.Parse(Console.ReadLine());
+
+                Console.Write($"Enter Employee {i + 1} Name ");
+                string? Name = Console.ReadLine();
+
+                Console.Write($"Enter Employee {i+1} Salary ");
+                decimal Salary  = decimal.Parse(Console.ReadLine()?? "3000");
+
+                Console.Write($"Enter Employee {i+1} Gender (Male - Female) ");
+                Gender Gender = Enum.Parse<Gender>(Console.ReadLine() ?? "Male",true);
+
+                Console.Write("How Many Privileges Employee Have ? : ");
+                int cnt = int.Parse(Console.ReadLine() ?? "0");
+                Console.WriteLine();
+                if (cnt > 0) 
+                {
+                    SecurityLevels Prvivileges = 0;
+                    for (int j = 0; j < cnt; j++)
+                    {
+                        Console.Write("Enter Employee Privilege [Guest  , Developer , Secertary , DBA ] : ");
+                        SecurityLevels Prv = Enum.Parse<SecurityLevels>(Console.ReadLine(), true);
+                        Prvivileges |= Prv;
+                    }
+
+                    Employees[i] = new Employee (id , Name , Salary , Gender , Prvivileges);
+                }
+                Console.Clear();
+
+            }
+            #endregion
+
         }
     }
 }
