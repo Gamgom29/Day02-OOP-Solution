4737b03291172e3880a55f8b467d15716914f991
Gamgom29 <abdogamgom2015@gmail.com>
2024-07-12 16:12:48 +0300

    video01 Class

---
+++ Demo/Car.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    class Car
+    {
+		#region Attributes
+		private int id  = default;
+		private string? model = default;
+		private double speed = default;
+		#endregion
+
+		#region Properties
+		public int Id
+		{
+			get { return id; }
+			set { id = value; }
+		}
+		public string? Model
+		{
+			get { return model; }
+			set { model = value; }
+		}
+		public double Speed
+		{
+			get { return speed; }
+			set { speed = value; }
+		}
+		#endregion
+
+		// if you define constructor , compiler will no longer generate empty constructor
+
+		/* public Car()
+         {
+
+         }*/
+        public Car(int id ,  string model , double speed)
+        {
+            Console.WriteLine("Ctor 01");
+            this.id = id;
+			this.model = model;	
+			this.speed = speed;
+            
+        }
+		public Car(int id, string model):this (id , model , 180) // Constructor Chaining
+        {
+            Console.WriteLine("Ctor 02");
+			//this.id = id;
+			//this.model = model;
+			//this.speed = 180;
+            
+        }
+        public Car(int id):this(id, "Hyndai Elntra 2023" , 200)
+        {
+            Console.WriteLine("Ctor 03");
+            /*this.id=id;
+			this.model = "Hyndai Elntra 2023";
+			this.speed = 200;*/
+        }
+
+        public override string ToString()
+        {
+            return $"Car Id {id} \nModel: {model} \nSpeed: {speed}";	
+        }
+
+
+    }
+}
--- Demo/Program.cs
+++ Demo/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Video01
+            //Car C1;
+
+            // C1 = new Car(123 , "Kia Cerato 2021" , 220);
+            // C1 = new Car(888 , "Renault Megane 2022");
+            // C1 = new Car(555);
+
+            /// new 
+            /// 1. Allocate required number of bytes for the allocated object 
+            /// 2.initialize the allocated bytes for each and every attribute with default value
+            /// 3.call user-defined constructor
+            /// 4. assign the address to the reference C2
+            /// 
+
+            //Console.WriteLine(C1.ToString()); 
+            #endregion
         }
     }
 }
